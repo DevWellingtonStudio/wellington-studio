aa8763bc34b18b8b4c7a4cfa01bccfa6e1d9fcab
steelbridge-io <chris@steelbridge.io>
2021-01-29 23:18:54 -0800

    added hero video

---
+++ lib/hero.php
@@ -0,0 +1,22 @@
+<?php
+	/** Hero images, carousel, video */
+
+add_action('wp_head', 'add_hero_video');
+function add_hero_video() {
+	echo '
+		<section>
+		<div class="overlay"></div>
+			<video playsinline="playsinline" autoplay="autoplay" muted="muted" loop="loop">
+			<source src="https://storage.googleapis.com/coverr-main/mp4/Mt_Baker.mp4" type="video/mp4">
+			</video>
+			<div class="container h-100">
+			<div class="d-flex h-100 text-center align-items-center">
+			<div class="w-100 text-white">
+			<h1 class="display-3">Video Header</h1>
+			<p class="lead mb-0">Text Goes Here</p>
+			</div>
+			</div>
+			</div>
+		</section>
+	';
+}
