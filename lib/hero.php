30f32c123705d95411ef2796e727243f71c12d22
steelbridge-io <chris@steelbridge.io>
2021-02-27 22:59:55 -0800

    Added slider alt

--- lib/hero.php
+++ lib/hero.php
@@ -12,14 +12,24 @@ function add_hero_video() {
 	$hero_video_title = get_theme_mod('hero_video_title', $default);
 	$hero_video_text  = get_theme_mod('hero_video_text', $default);
 	$wst_slider1	    = get_theme_mod('wst_slider1', $default );
+	$slider1_id       = attachment_url_to_postid($wst_slider1);
+	$slider1_alt      = get_post_meta( $slider1_id, '_wp_attachment_image_alt', true );
 	$slider1title	    = get_theme_mod('slider1title', $default );
 	$slider1text		  = get_theme_mod('slider1text', $default );
 	$wst_slider2	    = get_theme_mod('wst_slider2', $default );
+	$slider2_id       = attachment_url_to_postid($wst_slider2);
+	$slider2_alt      = get_post_meta( $slider2_id, '_wp_attachment_image_alt', true );
 	$slider2title	    = get_theme_mod('slider2title', $default );
 	$slider2text		  = get_theme_mod('slider2text', $default );
 	$wst_slider3	    = get_theme_mod('wst_slider3', $default );
+	$slider3_id       = attachment_url_to_postid($wst_slider3);
+	$slider3_alt      = get_post_meta( $slider3_id, '_wp_attachment_image_alt', true );
 	$slider3title	    = get_theme_mod('slider3title', $default );
 	$slider3text		  = get_theme_mod('slider3text', $default );
+
+
+	$slider1_id = attachment_url_to_postid($wst_slider1);
+	$slider1_alt  = get_post_meta( $slider1_id, '_wp_attachment_image_alt', true );
 
 	if($hero_url !== $default) {
 		if ( ! is_active_sidebar( 'home-featured' ) && is_front_page() ) {
@@ -61,7 +71,7 @@ echo '
 		echo ' </ol>
 				    <div class="carousel-inner">
 				      <div id="slider-one" class="carousel-item active">
-				        <img src="' . $wst_slider1 . '" class="d-block w-100" alt="">
+				        <img src="' . $wst_slider1 . '" class="d-block w-100" alt="'. $slider1_alt .'">
 				        <div class="carousel-caption d-md-block">
 				          <h2 class="slider-one-title">' . $slider1title  . '</h2>
 				          <p class="slider-one-text lead">' . $slider1text	 . '</p>
@@ -69,7 +79,7 @@ echo '
 				      </div>';
 						  if($wst_slider2 !== $default) {
 				       echo ' <div id="slider-two" class="carousel-item">
-				         <img src="' . $wst_slider2 . '" class="d-block w-100" alt="">
+				         <img src="' . $wst_slider2 . '" class="d-block w-100" alt="'. $slider2_alt .'">
 				         <div class="carousel-caption d-md-block">
 				           <h2 class="slider-two-title">' . $slider2title  . '</h2>
 				           <p class="slider-two-text lead">' .  $slider2text  . '</p>
@@ -78,7 +88,7 @@ echo '
 						  }
 						 if($wst_slider3 !== $default) {
 				       echo ' <div id="slider-three" class="carousel-item">
-				         <img src="' . $wst_slider3 . '" class="d-block w-100" alt="...">
+				         <img src="' . $wst_slider3 . '" class="d-block w-100" alt="'. $slider3_alt .'">
 				         <div class="carousel-caption d-md-block">
 				           <h2 class="slider-three-title">' . $slider3title . '</h2>
 				           <p class="slider-three-text lead">' . $slider3text  . '</p>
